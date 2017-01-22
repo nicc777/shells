e09a1b513254922ad255fdf9c52d2e01ab1e35e3
Nicolaas Coetzee <info@oculusd.com>
2017-01-22 11:33:52 +0200

    Added an important note to the deric script

--- common/deric.sh
+++ common/deric.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
+# Refer to https://github.com/nicc777/deric - I still need to add this to the public Docker library
+
 xhost +localhost
 
-if [ -z ${DERIC_HOME+x} ]; then 
+if [ -z ${DERIC_HOME+x} ]; then
 	export DERIC_HOME=$HOME/Projects
-	echo "DERIC_HOME was not set - using $DERIC_HOME"; 
+	echo "DERIC_HOME was not set - using $DERIC_HOME";
 fi
 
 docker run -ti --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $DERIC_HOME:/home/developer deric
-
-
